71cbc4f50140a548e0f58f9a32d266aaf3059f15
Yosuke Sugishita <yosuke.sugi@gmail.com>
2015-11-28 11:13:51 -0800

    put the conversion code into a function.

--- SoundVision/FirstViewController.mm
+++ SoundVision/FirstViewController.mm
@@ -8,6 +8,7 @@
 float t = 0;
 int x = 64; // set x = y = 64 when using Peter's strings.
 int y = 64;
+float *matrix_to_play = new float[x*y];
 float *phases = new float[y];
 int state = 0; // TODO: Fix this to an enum.
 
@@ -83,6 +84,14 @@ NSArray *peterStrings = @[  /* N x N pixels, 16 grey levels a,...,p */
     @"aappppppppppppppaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"];
 
 
+float *frequencies = new float[y];
+float frequency_max = 5000.0;
+float frequency_min = 500.0;
+const float T = 1.0; // time length of one cycle
+const float amplitude = 0.05; // the maximum amplitude we can use seems to be like 0.05.  I'm not 100% sure on this though.
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -99,70 +108,33 @@ NSArray *peterStrings = @[  /* N x N pixels, 16 grey levels a,...,p */
 - (IBAction)togglePlay:(UIButton *)selectedButton
 {
     if (state == 0){
-        for (int i = 0; i < y; i++){
-            phases[i] = 0.0;
-        }
-        t = 0;
-        [self.audioManager play];
+        [self playMatrix:matrix_to_play x_length: x y_length: y];
         [selectedButton setTitle:NSLocalizedString(@"Stop Sound Vision", nil) forState:0];
         state = 1;
     } else {
         [self.audioManager pause];
-        for (int i = 0; i < y; i++){
-            phases[i] = 0.0;
-        }
-        t = 0;
         [selectedButton setTitle:NSLocalizedString(@"Start Sound Vision", nil) forState:0];
         state = 0;
     }
 }
 
-- (void)viewWillAppear:(BOOL)animated
+
+// this function plays the matrix
+- (void)playMatrix: (float*) matrix
+          x_length: (int) x_len
+          y_length: (int) y_len
 {
-    [super viewWillAppear:animated];
-    
     __weak FirstViewController * wself = self;
-    
-    self.audioManager = [Novocaine audioManager];
-    
-    const float T = 1.0; // time length of one cycle
-    const float amplitude = 0.05; // the maximum amplitude we can use seems to be like 0.05.  I'm not 100% sure on this though.
-    
-    float *frequencies = new float[y];
-    float frequency_max = 5000.0;
-    float frequency_min = 500.0;
-    frequencies[0] = frequency_max;
-    float mult_rate = pow( frequency_min / frequency_max, 1/float(y));
-    for (int i = 1; i < y; i++) {
-        frequencies[i] = frequencies[i - 1] * mult_rate;
-        //        frequencies[i] = frequency_max - i * frequency_diff / (y - 1);
+    for (int i = 0; i < y; i++){
+        phases[i] = 0.0;
     }
-    
-//    // IDENTITY
-//    float *matrix = new float[x*y];
-//    for (int i = 0; i < x*y; i++){
-//        matrix[i] = 0.0;
-//    }
-//    for (int i = 0; i < y; i++){
-//        matrix[x * i + i] = 1.0;
-//    }
-    
-    // Peter's strings (it's the B&W drawing here: https://www.seeingwithsound.com/im2sound.htm)
-    float *matrix = new float[x*y];
-    for( int x_ind = 0 ; x_ind < x ; x_ind++ ){
-        for( int y_ind = 0 ; y_ind < y ; y_ind++ ){
-            // this way, we get 'p' = 15 and 'a' = 0.
-            float char_converted = (float)([peterStrings[y_ind] characterAtIndex:x_ind] - 'a') / 15.0;
-            matrix[x_ind + y_ind * x] = char_converted;
-        }
-    }
-    
-    
+    t = 0;
+
     [self.audioManager setOutputBlock:^(float *data, UInt32 numFrames, UInt32 numChannels)
      {
          //         NSLog(@"Time: %f", t);
          float samplingRate = wself.audioManager.samplingRate;
-
+         
          if (t + 2.0 / samplingRate > T){
              [wself.audioManager pause];
              //             free(matrix);
@@ -200,8 +172,43 @@ NSArray *peterStrings = @[  /* N x N pixels, 16 grey levels a,...,p */
              t += 1.0 / samplingRate;
          }
      }];
+    [self.audioManager play];
+
+}
+
+
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
     
-//    [self.audioManager play];
+    self.audioManager = [Novocaine audioManager];
+    frequencies[0] = frequency_max;
+    float mult_rate = pow( frequency_min / frequency_max, 1/float(y));
+    for (int i = 1; i < y; i++) {
+        // exponential scale
+        frequencies[i] = frequencies[i - 1] * mult_rate;
+        
+        // linear scale
+        //        frequencies[i] = frequency_max - i * frequency_diff / (y - 1);
+    }
+    
+//    // IDENTITY
+//    for (int i = 0; i < x*y; i++){
+//        matrix_to_play[i] = 0.0;
+//    }
+//    for (int i = 0; i < y; i++){
+//        matrix_to_play[x * i + i] = 1.0;
+//    }
+    
+    // Peter's strings (it's the B&W drawing here: https://www.seeingwithsound.com/im2sound.htm)
+    for( int x_ind = 0 ; x_ind < x ; x_ind++ ){
+        for( int y_ind = 0 ; y_ind < y ; y_ind++ ){
+            // this way, we get 'p' = 15 and 'a' = 0.
+            float char_converted = (float)([peterStrings[y_ind] characterAtIndex:x_ind] - 'a') / 15.0;
+            matrix_to_play[x_ind + y_ind * x] = char_converted;
+        }
+    }
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
