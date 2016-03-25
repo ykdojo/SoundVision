e0898a08d7936f806b20647418f2858072f06adf
Yosuke Sugishita <yosuke.sugi@gmail.com>
2016-03-25 03:17:15 -0700

    repeating method, it's working but not quite

--- SoundVision/FirstViewController.mm
+++ SoundVision/FirstViewController.mm
@@ -14,8 +14,8 @@ float *matrix_to_play = new float[x*y];
 int state = 0; // TODO: Fix this to an enum.
 
 float *frequencies = new float[y];
-float frequency_max = 5000.0;
-float frequency_min = 500.0;
+float frequency_max;
+float frequency_min;
 const float T = 1.05; // time length of one cycle
 const float amplitude = 0.05; // the maximum amplitude we can use seems to be like 0.05.  I'm not 100% sure on this though.
 
@@ -103,7 +103,7 @@ NSArray *peterStrings = @[  /* N x N pixels, 16 grey levels a,...,p */
     frequency_min = self.minFrequencySlider.value = 500.0;
     self.minFrequencyVal.text = [NSString stringWithFormat:@"%i Hertz", (int)roundf(frequency_min)];
     
-    frequency_max = self.maxFrequencySlider.value = 5000.0;
+    frequency_max = self.maxFrequencySlider.value = 700.0;
     self.maxFrequencyVal.text = [NSString stringWithFormat:@"%i Hertz", (int)roundf(frequency_max)];
     
     t = self.speedSlider.value;
@@ -155,8 +155,7 @@ NSArray *peterStrings = @[  /* N x N pixels, 16 grey levels a,...,p */
 
 // convert the x index so that if it's -1 (too much on the left), it becomes (xMax - 1) (on the right most column)
 // and if we have xMax (too much on the right), it becomes 0 (on the left most column)
-- (int)convertIndex:
-(int) xIndex
+- (int) convertIndex: (int) xIndex
            x_length: (int) x_len
 {
     if (xIndex == -1) {
@@ -190,95 +189,53 @@ NSArray *peterStrings = @[  /* N x N pixels, 16 grey levels a,...,p */
      {
          float samplingRate = wself.audioManager.samplingRate;
 
-         // +3.0 helps us get rid of the noise that we don't want at the end.
-         if (t + 4.0 / samplingRate * numFrames >= T){
-             [wself.audioManager pause];
-             state = 0;
-             [wself.playButton setTitle:NSLocalizedString(@"Start Sound Vision", nil) forState:0];
-         }
-
          for (int i=0; i < numFrames; ++i)
          {
              // numFramesTotal = # number of frames for all the columns
-             long numFramesTotal = samplingRate * T;
+             int NF = (int)(samplingRate * T); // number of frames total
+             // I want numFramesTotal to be divisible by sampling Rate
+             int numFramesTotal = NF - NF % (int)samplingRate;
              
              // m = # frames per column
              long m = (long) (numFramesTotal / x_len);
-             float r = 1.0 * current_frame / (floor (T*samplingRate) - 1);
-             float theta_2 = (r - 0.5) * 2 * M_PI / 3;
-             float head_size = 0.2; // in meters, I think
-             float x_distance = 0.5 * head_size * (theta_2 + sin(theta_2));
-             float v = 340; // speed of sound, 340 m/s
-             float tl = t;
-             float tr = t;
-             tr += x_distance / v;
              
              int x_i = current_frame / m;
+             if (x_i >= x_len){
+                 x_i = x_len - 1;
+             }
              float q = 1.0 * (current_frame % m) / (m - 1);
              float q2 = 0.5*q*q;
 
-             // go back to the beginning right before current_frame
-             //  reaches numFramesTotal
-             // (that's what the % is for here yo)
-             current_frame = (current_frame + 1) % numFramesTotal;
-
-             float sl = 0;
-             float sr = 0;
+             float s = 0;
              for (int y_i = 0; y_i < y_len; y_i++){
                      float tmp_amplitude;
-                     if (x_i == 0){
-                         tmp_amplitude =
-                            (1.0 - q2) * A[x_i + y_i*x_len]
-                         + q2 * A[(x_i + 1) + y_i*x_len];
-                     }
-                     else if (x_i == x_len - 1) {
-                         tmp_amplitude =
-                            (q2 - q +0.5) * A[(x_i - 1) + y_i*x_len]
-                         + (0.5 + q - q2) * A[x_i + y_i*x_len];
-                     }
-                     else {
-                         tmp_amplitude =
-                            (q2 - q +0.5) * A[(x_i - 1) + y_i*x_len]
-                         + (0.5+q-q*q) * A[x_i + y_i*x_len]
-                         + q2 * A[(x_i + 1) + y_i*x_len];
-                     }
-                    
-                     float theta_l = frequencies[y_i] * M_PI * 2 * tl;
-                     float theta_r = frequencies[y_i] * M_PI * 2 * tr;
-                     
-                     float x_abs = fabs(x_distance);
-                     float diffraction;
-                     if (v / frequencies[y - y_i - 1] > x_abs){
-                         diffraction = 1;
-                     } else {
-                         diffraction = v / (x_abs * frequencies[y - y_i - 1]);
-                     }
+                     int x_ind_left = [self convertIndex:(x_i - 1) x_length:x_len];
+                     int x_ind_right = [self convertIndex:(x_i + 1) x_length:x_len];
+                 
+                    // TODO: test if this part is correct!
+                   // I should always get the same result for this one for the same y_i
+                    // -> TEST IT!!!  (WORKING ON THIS NOW)
+                     tmp_amplitude =
+                        (q2 - q +0.5) * A[x_ind_left + y_i*x_len]
+                     + (0.5+q-q*q) * A[x_i + y_i*x_len]
+                     + q2 * A[x_ind_right + y_i*x_len];
+                 
+                     float theta = frequencies[y_i] * M_PI * 2 * t;
 
-                     float fade_l = 1;
-                     float fade_r = 1;
-
-//                     if (theta_2 < 0.0) { fade_l = 1.0; fade_r = diffraction; }
-//                     else { fade_l = diffraction; fade_r = 1; }
-                     
-                     fade_l *= (1.0 - 0.7*r);
-                     fade_r *= (0.3 + 0.7*r);
-
-                     sl += fade_l * tmp_amplitude * sin(theta_l + phases[y_i]);
-                     sr += fade_r * tmp_amplitude * sin(theta_r + phases[y_i]);
-//                     NSLog(@"phase: %f", phases[y_i]);
+                     s += tmp_amplitude * sin(theta + phases[y_i]);
              }
              
              // copying the same data for both channels (left and right speakers)
              for (int iChannel = 0; iChannel < numChannels; ++iChannel)
              {
-                 // left channel
-                 if(iChannel == 0) {
-                     data[i*numChannels + iChannel] = sl * amplitude;
-                 } else { // right channel
-                     data[i*numChannels + iChannel] = sr * amplitude;
-                 }
+                data[i*numChannels + iChannel] = s * amplitude;
              }
-             t = current_frame * 1.0 / samplingRate;
+             
+             // go back to the beginning right before current_frame
+             //  reaches numFramesTotal
+             // (that's what the % is for here yo)
+             current_frame = (current_frame + 1) % numFramesTotal;
+             t += 1.0 / samplingRate;
          }
      }];
     [self.audioManager play];
@@ -301,13 +258,25 @@ NSArray *peterStrings = @[  /* N x N pixels, 16 grey levels a,...,p */
         //        frequencies[i] = frequency_max - i * frequency_diff / (y - 1);
     }
     
-//    // IDENTITY
+    // IDENTITY
     for (int i = 0; i < x*y; i++){
         matrix_to_play[i] = 0.0;
     }
     for (int i = 0; i < y; i++){
         matrix_to_play[x * i + i] = 1.0;
     }
+
+    
+//    //   A single horizontal line
+//    for (int i = 0; i < x*y; i++){
+//        matrix_to_play[i] = 0.0;
+//    }
+//    for (int i = 0; i < x; i++){
+//        matrix_to_play[i + (y*3/4) * x] = 1.0;
+//    }
+
+    
+    
     
 ////     Peter's strings (it's the B&W drawing here: https://www.seeingwithsound.com/im2sound.htm)
 //    for( int x_i = 0 ; x_i < x ; x_i++ ){
